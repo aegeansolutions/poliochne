8efa49c559b628fe92e4d90082709c6de6edd16a
glinard <linardakisgiorgos@gmail.com>
2018-11-19 15:59:25 +0200

    third submit

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ddd',
+    backgroundColor: '#eee',
   },
   welcome: {
     fontSize: 20,
