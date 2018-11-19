23d9a0fbb02500f5ea31e874018165a8793e8007
giokonstantakis <giokonstantakis@gmail.com>
2018-11-19 15:24:32 +0200

    color change

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000000',
+    backgroundColor: '#ffffff',
   },
   welcome: {
     fontSize: 20,
